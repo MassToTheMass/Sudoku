b94e062024d443f56023675bb1f77dfa72be7af6
Mason Christopherson <masonxiv.14@icloud.com>
2025-03-05 19:46:26 +0000

    some functions, need to make set to check off values that have been checked

--- grid.hpp
+++ grid.hpp
@@ -21,66 +21,83 @@ class Grid {
             }
         };
 
-        Node arr[9][9];
-        std::stack<std::pair<int, int>> location_stack;
-        std::pair<int, int> origin = {0, 0};
+        Node* arr[9][9];
+        std::stack<Node*> location_stack;
 
 
     public:
-        Grid() {
-            location_stack.push(origin);
-        }
+        Grid() {}
 
         void generateSudokuGrid(int seed=time(0)) {
 
             srand(seed);
+            for (int i = 8; i > 0; --i) {
+                for (int j = 8; j > 0; --j) {
+                    location_stack.push(new Node(i, j));
+                }
+            }
+            solveGrid();
 
-            for (int i = 0; i != 3; ++i) {
-                for (int j = 0; j != 3; ++j) {
-                    
-                    std::set<int> occurrence_checker;
-                    int chosen_number = 0;
+        }
+    
+        bool checkRow(Node* n) {
 
-                    for (int k = 0; k != 3; ++k) {
-                        for (int l = 0; l != 3; ++l) {
+            for (int i = 0; i < 9; ++i) {
+                if (i == n->first) {} else {
+                    if (arr[i][n->second]->value == n->value) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
 
-                            do {
-                                chosen_number = rand() % 9 + 1;
-                            } while (!occurrence_checker.insert(chosen_number).second);
+        bool checkColumn(Node* n) {
 
-                            arr[i * 3 + k][j * 3 + l] = chosen_number;
+            for (int i = 0; i < 9; ++i) {
+                if (i == n->second) {} else {
+                    if (arr[n->second][i]->value == n->value) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        
+        bool checkBox(Node* n) {
 
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    if (arr[i][j] == n) {} else {
+                        if (arr[i][j]->value == n->value) {
+                            return false;
                         }
                     }
                 }
             }
-
-            std::cout << arr << std::endl;
-            
-            return;
+            return true;
         }
+
     
-    bool solveGrid() {
-        std::pair<int, int> current = location_stack.top();
-        int value = arr[current.first][current.second];
+        bool solveGrid() {
+            Node* current = location_stack.top();
 
-        for (int i = 0; i < 9; ++i) {
-            if (i != current.first) {
-                if (arr[i][current.second] == value) {
+            // need to make a set to check numbers
 
+            current->value = rand() % 9 + 1; // random between 1 and 9
+            if (checkRow(current), checkColumn(current), checkBox(current)) {
+                location_stack.pop();
+            }
+        }
+
+        void printGrid() {
+            for (int i = 0; i < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    std::cout << "[" << arr[i][j] << "]";
                 }
+                std::cout << std::endl;
             }
         }
-    }
-
-    void printGrid() {
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                std::cout << "[" << arr[i][j] << "]";
-            }
-            std::cout << std::endl;
-        }
-    }
 
     friend std::ostream& operator<<(std::ostream& os, const Grid g) {}
 
